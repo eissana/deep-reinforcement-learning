59038b2f1ebcb13995c41a0bd45957f749e82c69
Eissa Nematollahi <eissana@gmail.com>
2019-12-13 19:51:02 -0500

    add CartPole code

---
+++ app/cartpole_random.py
@@ -0,0 +1,25 @@
+import gym
+
+
+def play(env):
+    total_reward = 0.0
+    total_steps = 0
+
+    while True:
+        action = env.action_space.sample()
+        _, reward, done, _ = env.step(action)
+        total_reward += reward
+        total_steps += 1
+
+        if done:
+            break
+
+    return total_reward, total_steps
+
+if __name__ == "__main__":
+    env = gym.make('CartPole-v0')
+    env.reset()
+
+    total_reward, total_steps = play(env)
+
+    print(f"episode done in {total_steps} steps; total reward: {total_reward}")
